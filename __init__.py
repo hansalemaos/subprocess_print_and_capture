bfaa4d3e158e403a8eab96a4ff3e7cf8a0ea843f
Hans Alemão <aulasparticularesdealemaosp@gmail.com>
2022-12-01 11:35:53 -0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,17 +1,25 @@
 import os
+import random
 import subprocess
+from time import sleep
+
 import keyboard as keyboard__
+import kthread
 import psutil
 
 
 def execute_subprocess(
-    cmd: str, exit_keys: str = "ctrl+e", end_of_printline: str = ""
+    cmd: str, exit_keys: str = "ctrl+e", end_of_printline: str = "", timeout=None
 ) -> list:
     popen = None
 
+    t= None
     def run_subprocess(cmd):
+        nonlocal t
         nonlocal popen
-
+        def killer():
+            sleep(timeout)
+            kill_process()
         def kill_process():
             nonlocal popen
             try:
@@ -32,6 +40,10 @@ def execute_subprocess(
                     keyboard__.unhook_all_hotkeys()
                 except Exception:
                     pass
+
+        if timeout is not None:
+            t = kthread.KThread(target=killer, name=str(random.randrange(1, 100000000000)))
+            t.start()
 
         try:
             popen = subprocess.Popen(
@@ -89,6 +101,12 @@ def execute_subprocess(
             keyboard__.unhook_all_hotkeys()
         except Exception:
             pass
+    try:
+        if t is not None:
+            if t.is_alive():
+                t.kill()
+    except Exception:
+        pass
     return proxyresults
 
 
@@ -285,4 +303,248 @@ def execute_subprocess_multiple_commands_v2(
             keyboard__.unhook_all_hotkeys()
         except Exception:
             pass
+    return proxyresults
+
+
+def execute_subprocess_multiple_commands_with_timeout_str(
+    cmd: str, subcommands: list, exit_keys: str = "ctrl+x", end_of_printline: str = "", print_output=True, timeout=None
+) -> list:
+    if isinstance(subcommands, str):
+        subcommands = [subcommands]
+    elif isinstance(subcommands, tuple):
+        subcommands = list(subcommands)
+    popen = None
+    t= None
+    def run_subprocess(cmd):
+        nonlocal t
+        nonlocal popen
+
+        def killer():
+            sleep(timeout)
+            kill_process()
+
+        def kill_process():
+            nonlocal popen
+            try:
+                print("Killing the process")
+                p = psutil.Process(popen.pid)
+                p.kill()
+                try:
+                    if exit_keys in keyboard__.__dict__["_hotkeys"]:
+                        keyboard__.remove_hotkey(exit_keys)
+                except Exception:
+                    try:
+                        keyboard__.unhook_all_hotkeys()
+                    except Exception:
+                        pass
+            except Exception:
+                try:
+                    keyboard__.unhook_all_hotkeys()
+                except Exception:
+                    pass
+
+        if exit_keys not in keyboard__.__dict__["_hotkeys"]:
+            keyboard__.add_hotkey(exit_keys, kill_process)
+
+        DEVNULL = open(os.devnull, "wb")
+        try:
+            popen = subprocess.Popen(
+                cmd,
+                stdin=subprocess.PIPE,
+                stdout=subprocess.PIPE,
+                universal_newlines=True,
+                stderr=DEVNULL,
+                shell=False,
+            )
+
+            for subcommand in subcommands:
+                if isinstance(subcommand, bytes):
+                    subcommand = subcommand.rstrip(b"\n") + b"\n"
+
+                    subcommand = subcommand.decode("utf-8", "replace")
+                else:
+                    subcommand = subcommand.rstrip("\n") + "\n"
+
+                popen.stdin.write(subcommand)
+
+            popen.stdin.close()
+
+            if timeout is not None:
+                t = kthread.KThread(target=killer, name=str(random.randrange(1,100000000000)))
+                t.start()
+
+            for stdout_line in iter(popen.stdout.readline, ""):
+                try:
+                    yield stdout_line
+                except Exception as Fehler:
+                    continue
+            popen.stdout.close()
+            return_code = popen.wait()
+        except Exception as Fehler:
+            print(Fehler)
+            try:
+                popen.stdout.close()
+                return_code = popen.wait()
+            except Exception as Fehler:
+                yield ""
+
+    proxyresults = []
+    try:
+        for proxyresult in run_subprocess(cmd):
+            proxyresults.append(proxyresult)
+            if print_output:
+                print(proxyresult, end=end_of_printline)
+    except KeyboardInterrupt:
+        try:
+            p = psutil.Process(popen.pid)
+            p.kill()
+            popen = None
+        except Exception as da:
+            print(da)
+
+    try:
+        if popen is not None:
+            p = psutil.Process(popen.pid)
+            p.kill()
+    except Exception as da:
+        pass
+
+    try:
+        if exit_keys in keyboard__.__dict__["_hotkeys"]:
+            keyboard__.remove_hotkey(exit_keys)
+    except Exception:
+        try:
+            keyboard__.unhook_all_hotkeys()
+        except Exception:
+            pass
+    try:
+        if t is not None:
+            if t.is_alive():
+                t.kill()
+    except Exception:
+        pass
+    return proxyresults
+
+
+def execute_subprocess_multiple_commands_with_timeout_bin(
+    cmd: str, subcommands: list, exit_keys: str = "ctrl+x", end_of_printline: str = "", print_output=True, timeout=None
+) -> list:
+    if isinstance(subcommands, str):
+        subcommands = [subcommands]
+    elif isinstance(subcommands, tuple):
+        subcommands = list(subcommands)
+    popen = None
+    t= None
+    def run_subprocess(cmd):
+        nonlocal t
+        nonlocal popen
+
+        def killer():
+            sleep(timeout)
+            kill_process()
+
+        def kill_process():
+            nonlocal popen
+            try:
+                print("Killing the process")
+                p = psutil.Process(popen.pid)
+                p.kill()
+                try:
+                    if exit_keys in keyboard__.__dict__["_hotkeys"]:
+                        keyboard__.remove_hotkey(exit_keys)
+                except Exception:
+                    try:
+                        keyboard__.unhook_all_hotkeys()
+                    except Exception:
+                        pass
+            except Exception:
+                try:
+                    keyboard__.unhook_all_hotkeys()
+                except Exception:
+                    pass
+
+        if exit_keys not in keyboard__.__dict__["_hotkeys"]:
+            keyboard__.add_hotkey(exit_keys, kill_process)
+
+        DEVNULL = open(os.devnull, "wb")
+        try:
+            popen = subprocess.Popen(
+                cmd,
+                stdin=subprocess.PIPE,
+                stdout=subprocess.PIPE,
+                universal_newlines=False,
+                stderr=DEVNULL,
+                shell=False,
+            )
+
+            for subcommand in subcommands:
+                if isinstance(subcommand, str):
+                    subcommand = subcommand.rstrip("\n") + "\n"
+
+                    subcommand = subcommand.encode()
+                else:
+                    subcommand = subcommand.rstrip(b"\n") + b"\n"
+
+                popen.stdin.write(subcommand)
+
+            popen.stdin.close()
+
+            if timeout is not None:
+                t = kthread.KThread(target=killer, name=str(random.randrange(1,100000000000)))
+                t.start()
+
+            for stdout_line in iter(popen.stdout.readline, b""):
+                try:
+                    yield stdout_line
+                except Exception as Fehler:
+                    continue
+            popen.stdout.close()
+            return_code = popen.wait()
+        except Exception as Fehler:
+            print(Fehler)
+            try:
+                popen.stdout.close()
+                return_code = popen.wait()
+            except Exception as Fehler:
+                yield ""
+
+    proxyresults = []
+    try:
+        for proxyresult in run_subprocess(cmd):
+            proxyresults.append(proxyresult)
+            if print_output:
+                try:
+                    print(f'{proxyresult!r}', end=end_of_printline)
+                    print('')
+                except Exception:
+                    pass
+    except KeyboardInterrupt:
+        try:
+            p = psutil.Process(popen.pid)
+            p.kill()
+            popen = None
+        except Exception as da:
+            print(da)
+
+    try:
+        if popen is not None:
+            p = psutil.Process(popen.pid)
+            p.kill()
+    except Exception as da:
+        pass
+
+    try:
+        if exit_keys in keyboard__.__dict__["_hotkeys"]:
+            keyboard__.remove_hotkey(exit_keys)
+    except Exception:
+        try:
+            keyboard__.unhook_all_hotkeys()
+        except Exception:
+            pass
+    try:
+        if t is not None:
+            if t.is_alive():
+                t.kill()
+    except Exception:
+        pass
     return proxyresults
